b67770d39cbbdaa4fac88c2c86aa2e3a01186438
LautaroRolon <lrolon@sysworld.com.ar>
2023-10-30 20:22:07 -0300

    Agrego ruta de usuarios

--- src/servidor.js
+++ src/servidor.js
@@ -1,13 +1,12 @@
 require('dotenv').config();
 
 const express = require('express');
-const app = express();
+const usuarioRouter = require('./routes/usuarioRoutes.js');
 
+const app = express();
 const PORT = 3000;
 
-app.get('/', (req, res) => {
-    res.send('Hola mundo!');
-});
+app.use(usuarioRouter);
 
 app.listen(PORT, () => {
     console.log(`Servidor corriendo en el puerto ${PORT}`);
