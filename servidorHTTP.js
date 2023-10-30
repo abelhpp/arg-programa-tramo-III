61a75b5162212de21e0213144b2516d9e24d9b65
LautaroRolon <lrolon@sysworld.com.ar>
2023-10-30 19:03:44 -0300

    Subo estructura del proyecto

--- servidorHTTP.js
+++
@@ -1,9 +0,0 @@
-const http = require('node:http');
-
-const PORT = 3000;
-
-const servidor = http.createServer();
-
-servidor.listen(PORT, function() {
-    console.log(`Servidor corriendo en el puerto ${PORT}`);
-});
