bcb8b4bf5791f7ffbd9296562fd3e5f72e6d372e
LautaroRolon <lrolon@sysworld.com.ar>
2023-11-02 19:37:22 -0300

    Agrego subida de archivos

--- src/servidor.js
+++ src/servidor.js
@@ -2,18 +2,23 @@ require('dotenv').config();
 
 const express = require('express');
 const bodyParser = require('body-parser');
+const fileUpload = require('express-fileupload');
 
 const usuarioRouter = require('./routes/usuarioRoutes.js');
 const autenticacionRouter = require('./routes/autenticacionRoutes.js');
+const archivoRouter = require('./routes/archivoRouter.js');
 
 const app = express();
 const PORT = 3000;
 
 // Middleware
 app.use(bodyParser.json());
+app.use(fileUpload());
 
+// Rutas
 app.use(usuarioRouter);
 app.use(autenticacionRouter);
+app.use(archivoRouter);
 
 app.listen(PORT, () => {
     console.log(`Servidor corriendo en el puerto ${PORT}`);
