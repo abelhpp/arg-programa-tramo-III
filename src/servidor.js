dc2ed787221416ad26e99debcd12d06bfd988dfd
LautaroRolon <lrolon@sysworld.com.ar>
2023-11-06 18:41:50 -0300

    Agrego servicio externo para obtener provincias

--- src/servidor.js
+++ src/servidor.js
@@ -7,6 +7,7 @@ const fileUpload = require('express-fileupload');
 const usuarioRouter = require('./routes/usuarioRoutes.js');
 const autenticacionRouter = require('./routes/autenticacionRoutes.js');
 const archivoRouter = require('./routes/archivoRouter.js');
+const georefRouter = require('./routes/georefRouter.js');
 
 const app = express();
 const PORT = 3000;
@@ -19,6 +20,7 @@ app.use(fileUpload());
 app.use(usuarioRouter);
 app.use(autenticacionRouter);
 app.use(archivoRouter);
+app.use(georefRouter);
 
 app.listen(PORT, () => {
     console.log(`Servidor corriendo en el puerto ${PORT}`);
